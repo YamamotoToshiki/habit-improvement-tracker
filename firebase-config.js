81582a74dea6eed0ffd0f0e4801e899c03ff8e77
YamamotoToshiki <yamamoto0toshiki@gmail.com>
2025-12-14 14:53:42 +0900

    Correction: firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -20,5 +20,8 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
+// Initialize Google Auth Provider
+const googleProvider = new GoogleAuthProvider();
+
 // Export initialized instances for use in other modules
-export { app, db, auth };
+export { app, db, auth, googleProvider };
